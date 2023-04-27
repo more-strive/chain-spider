84f358d254ddd8d2e8fa69b31e8e17e0a480d4b7
zhudawei <15972699417@163.com>
2023-04-27 14:05:53 +0800

    更新爬虫api接口

--- binance_spider.py
+++ binance_spider.py
@@ -1,60 +1,50 @@
 import common
 from lxml import etree
 
-def binanceSpider():
-  url = 'https://bscscan.com/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c#balances'
-  # proxy = common.get_proxy().get("proxy")
-  
-  # retry_count = 5
-  # # with open('./html/binance.html', 'w', encoding='utf-8') as wf:
-  # #   wf.write(response.text)
-  # while retry_count > 0:
-  #   try:
-  #     response = requests.get(url, proxies={"http://{}".format(proxy)}, headers=headers)
-  #   except Exception:
-  #     retry_count -= 1
-  # common.delete_proxy(proxy)
-  # return None
+def binanceSpider(address='0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'):
+  url = 'https://bscscan.com/token/%s#balances' % address
+  data = {}
   response = common.get_response(url)
-  print('response:', response)
-  if not response:
-    return
+  if response.status_code != 200:
+    return data
+  # with open('./html/binance.html', 'w', encoding='utf-8') as wf:
+  #   wf.write(response.text)
   selecter = etree.HTML(response.text)  
   iconXpath = selecter.xpath('//*[@id="content"]/div[1]/div/div[1]/h1/img')
   if len(iconXpath) == 1:
     iconElement = iconXpath[0] 
     iconSrc = iconElement.attrib.get('src')
-    print('iconSrc:', iconSrc)
+    data['iconSrc'] = iconSrc
 
   overviewXpath = selecter.xpath('//*[@id="ContentPlaceHolder1_divSummary"]/div[1]/div[1]/div/div[1]/h2/span/b')
   if len(overviewXpath) == 1:
     overviewElement = overviewXpath[0]
     overviewText = overviewElement.text
-    print("overviewText:", overviewText)
+    data['overviewText'] = overviewText
 
   supplyXpath = selecter.xpath('//*[@id="ContentPlaceHolder1_divSummary"]/div[1]/div[1]/div/div[2]/div[2]/div[2]/span[1]')
   if len(supplyXpath) == 1:
     supplyElement = supplyXpath[0] 
     supplyText = supplyElement.text
-    print("supplyText:", supplyText) 
+    data['supplyText'] = supplyText 
 
   holdersXpath = selecter.xpath('//*[@id="ContentPlaceHolder1_tr_tokenHolders"]/div/div[2]/div/div')
   if len(holdersXpath) == 1:
     holdersElement = holdersXpath[0] 
     holdersText = holdersElement.text
-    print("holdersText:", holdersText.strip()) 
+    data['holdersText'] = holdersText.strip()
   
   decimalsXpath = selecter.xpath('//*[@id="ContentPlaceHolder1_trDecimals"]/div/div[2]')
   if len(decimalsXpath) == 1:
     decimalsElement = decimalsXpath[0] 
     decimalsText = decimalsElement.text
-    print("decimalsText:", decimalsText.strip()) 
+    data['decimalsText'] = decimalsText.strip()
   
   officialXpath = selecter.xpath('//*[@id="ContentPlaceHolder1_tr_officialsite_1"]/div/div[2]/a')
   if len(officialXpath) == 1:
     officialElement = officialXpath[0] 
     officialText = officialElement.attrib.get('href')
-    print("officialText:", officialText.strip()) 
+    data['officialText'] = officialText
 
   socialXpath = selecter.xpath('//*[@id="ContentPlaceHolder1_divSummary"]/div[1]/div[2]/div/div[2]/div[4]/div/div[2]/ul/li')
   if len(socialXpath) >= 0:
@@ -63,8 +53,9 @@ def binanceSpider():
       if len(socialSonXpath) == 1:
         socialSonElement = socialSonXpath[0]
         content = socialSonElement.attrib.get('data-original-title')
-        print('content:', content)
-    
+        value = content.split(': ')
+        data[value[0]] = value[1].strip()
+  return data
   
 if __name__ == '__main__':
-  binanceSpider()
+  print(binanceSpider())
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import uvicorn
-from fastapi import FastAPI, Depends
+import binance_spider
+import etherscan_spider
+import asyncio
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/binance")
-def binance():
-  return {"hello": 'binance'}
+@app.get("/api/chain/data")
+async def main(chain: str, address: str):
+  data = None
+  if chain == 'binance':
+    data = binance_spider.binanceSpider(address)
+  elif chain == 'etherscan':
+    data = etherscan_spider.etherscanSpider(address)
+  return {"chain": chain, 'address': address, 'data': data}
 
-@app.get("/etherscan")
-def etherscan():
-  return {"hello": 'etherscan'}
+
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app', host="192.168.1.103", port=8000, reload=True)
