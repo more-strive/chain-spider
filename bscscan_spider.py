26cff1fbb5f8bfa39811f2d930922c652c40019b
zhudawei <15972699417@163.com>
2023-05-05 14:56:15 +0800

    更新

--- bscscan_spider.py
+++ bscscan_spider.py
@@ -10,7 +10,7 @@ def bscscanSpider(address='0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'):
     return data
   with open('./html/bscscan.html', 'w', encoding='utf-8') as wf:
     wf.write(response.text)
-  
+  print('response.status_code:', response.status_code)
   if response.status_code != 200:
     return data
   # with open('./html/binance.html', 'w', encoding='utf-8') as wf:
