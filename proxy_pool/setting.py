4c408b191957855afa7bc42f3a77c52b4035e37a
zhudawei <15972699417@163.com>
2023-05-05 11:49:24 +0800

    更新代理验证

--- proxy_pool/setting.py
+++ proxy_pool/setting.py
@@ -58,9 +58,9 @@ PROXY_FETCHER = [
 
 # ############# proxy validator #################
 # 代理验证目标网站
-HTTP_URL = "http://httpbin.org"
+HTTP_URL = "http://bscscan.com"
 
-HTTPS_URL = "https://www.qq.com"
+HTTPS_URL = "https://bscscan.com"
 
 # 代理验证时超时时间
 VERIFY_TIMEOUT = 10
